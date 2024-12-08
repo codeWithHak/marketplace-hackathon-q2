14111218078a0866df3cc1b5286fea38939a85c7
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-12-08 18:27:36 +0500

    completed contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,36 @@
 import Image from 'next/image'
 
+const features = [
+    {
+      icon: '/images/trophy.png',
+      title: 'High Quality',
+      description: 'crafted from top materials'
+    },
+    {
+      icon: '/images/tick.png',
+      title: 'Warranty Protection',
+      description: 'Over 2 years'
+    },
+    {
+      icon: '/images/gift.png',
+      title: 'Free Shipping',
+      description: 'Order over 150 $'
+    },
+    {
+      icon: '/images/support.png',
+      title: '24 / 7 Support',
+      description: 'Dedicated support'
+    }
+  ]
+
 export default function ContactPage() {
   return (
-    <div className="max-w-7xl mx-auto px-4 py-16 font-poppins">
+    <>
+    <div className="max-w-6xl mx-auto px-4 py-16 font-poppins">
       {/* Header */}
       <div className="text-center mb-16 max-w-2xl mx-auto">
-        <h1 className="text-3xl md:text-4xl font-medium mb-4">Get In Touch With Us</h1>
-        <p className="text-gray-600 text-sm md:text-base">
+        <h1 className="text-3xl md:text-4xl font-poppinsSemiBold mb-4">Get In Touch With Us</h1>
+        <p className="text-gray-600 text-sm text-lightGray max-w-xl mx-auto">
           For More Information About Our Product & Services. Please Feel Free To Drop Us
           An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!
         </p>
@@ -16,12 +40,14 @@ export default function ContactPage() {
         {/* Contact Information */}
         <div className="space-y-8">
           {/* Address */}
-          <div className="flex items-start gap-8">
+          <div className="flex items-start gap-4">
             <div className="relative w-12 h-12 flex-shrink-0">
               <Image
                 src="/images/address.png"
                 alt="Address icon"
-                fill
+                height={20}
+                width={20}
+                quality={100}
                 className="object-contain"
               />
             </div>
@@ -34,12 +60,14 @@ export default function ContactPage() {
           </div>
 
           {/* Phone */}
-          <div className="flex items-start gap-8">
+          <div className="flex items-start gap-4">
             <div className="relative w-12 h-12 flex-shrink-0">
               <Image
                 src="/images/phone.png"
                 alt="Phone icon"
-                fill
+                height={20}
+                width={20}
+                quality={100}
                 className="object-contain"
               />
             </div>
@@ -53,12 +81,14 @@ export default function ContactPage() {
           </div>
 
           {/* Working Time */}
-          <div className="flex items-start gap-8">
+          <div className="flex items-start gap-4">
             <div className="relative w-12 h-12 flex-shrink-0">
               <Image
                 src="/images/time.png"
                 alt="Time icon"
-                fill
+                height={20}
+                width={20}
+                quality={100}
                 className="object-contain"
               />
             </div>
@@ -78,9 +108,10 @@ export default function ContactPage() {
             <div className="space-y-2">
               <label className="block text-sm">Your name</label>
               <input
+                
                 type="text"
                 placeholder="Abc"
-                className="w-full p-3 border rounded-lg"
+                className="w-full px-3 py-5 border border-lightGray rounded-lg"
               />
             </div>
 
@@ -89,7 +120,7 @@ export default function ContactPage() {
               <input
                 type="email"
                 placeholder="Abc@def.com"
-                className="w-full p-3 border rounded-lg"
+                className="w-full px-3 py-5 border border-lightGray rounded-lg"
               />
             </div>
 
@@ -98,7 +129,7 @@ export default function ContactPage() {
               <input
                 type="text"
                 placeholder="This is an optional"
-                className="w-full p-3 border rounded-lg"
+                className="w-full px-3 py-5 border border-lightGray rounded-lg"
               />
             </div>
 
@@ -107,19 +138,49 @@ export default function ContactPage() {
               <textarea
                 rows={4}
                 placeholder="Hi! I'd like to ask about"
-                className="w-full p-3 border rounded-lg resize-none"
+                className="w-full p-3 border border-lightGray rounded-lg resize-none"
               />
             </div>
 
             <button 
-              className="w-full bg-[#B88E2F] hover:bg-[#9d7829] text-white"
+              className="py-3 px-14 font-poppins text-xs bg-[#B88E2F] hover:bg-[#9d7829] text-white rounded-sm"
             >
               Submit
             </button>
           </form>
         </div>
       </div>
+      
     </div>
+    <div className="w-full bg-[#FAF3EA] mx-auto px-4 py-16 mt-6 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-7xl mx-auto">
+          {features.map((feature) => (
+            <div 
+              key={feature.title} 
+              className="flex items-center gap-4 pl-8 md:pl-0"
+            >
+              <div className="mb-4">
+                <Image
+                  src={feature.icon}
+                  alt={feature.title}
+                  width={60}
+                  height={60}
+                  quality={100}
+                />
+              </div>
+              <div className='flex flex-col'>
+              <h3 className="text-[#333333] text-xl font-semibold mb-2">
+                {feature.title}
+              </h3>
+              <p className="text-[#666666] text-base">
+                {feature.description}
+              </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 
