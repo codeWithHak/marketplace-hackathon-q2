d873cc3c987ba0e121cb0dcb39c0e1b32f24ec6b
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-12-23 19:20:51 +0500

    trying again to solve image error

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -4,10 +4,7 @@ import { client } from '../lib/client'
 
 const imageBuilder = createImageUrlBuilder(client)
 
-import { dataset, projectId } from '../env'
-
 // https://www.sanity.io/docs/image-url
-const builder = createImageUrlBuilder({ projectId, dataset })
 
 export const urlFor = (source: SanityImageSource) => {
   return imageBuilder.image(source).auto('format').fit('max')
