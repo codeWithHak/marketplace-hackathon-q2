caa2bdecc6a40f2bc18bfd7dcf38f286c0edbc89
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-12-23 19:35:58 +0500

    again trying again failing to deploy

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,17 +1,17 @@
 import { createClient } from 'next-sanity'
 
-if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
-  throw new Error('Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID')
-}
-
-if (!process.env.NEXT_PUBLIC_SANITY_DATASET) {
-  throw new Error('Missing environment variable: NEXT_PUBLIC_SANITY_DATASET')
-}
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: projectId || '',
+  dataset: dataset || '',
   apiVersion: '2023-05-03',
-  useCdn: process.env.NODE_ENV === 'production',
+  useCdn: false, // We'll set this to true in production
 })
+
+export const sanityConfig = {
+  projectId,
+  dataset,
+}
 
