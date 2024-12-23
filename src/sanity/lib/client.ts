af8f0d645b027598fca5bc3a2c428d81a37cadc9
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-12-23 19:57:31 +0500

    redeploying

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,18 +1,10 @@
 import { createClient } from 'next-sanity'
 
-if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
-  throw new Error('Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID')
-}
-
-if (!process.env.NEXT_PUBLIC_SANITY_DATASET) {
-  throw new Error('Missing environment variable: NEXT_PUBLIC_SANITY_DATASET')
-}
+import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2023-05-03',
-  useCdn: process.env.NODE_ENV === 'production',
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
-
-
