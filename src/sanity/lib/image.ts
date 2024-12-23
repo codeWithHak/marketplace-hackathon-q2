7536a10eafa8f562d1a3a76d90aed330af614669
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-12-23 19:54:24 +0500

    redeploying

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,15 +1,11 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import { client } from '../lib/client'
 
-const imageBuilder = createImageUrlBuilder(client)
+import { dataset, projectId } from '../env'
 
 // https://www.sanity.io/docs/image-url
+const builder = createImageUrlBuilder({ projectId, dataset })
 
 export const urlFor = (source: SanityImageSource) => {
-  return imageBuilder.image(source).auto('format').fit('max')
+  return builder.image(source)
 }
-
-
-
-
